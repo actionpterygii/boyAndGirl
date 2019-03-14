1708bb70122f028a84e65ec11ae4292120152650
actionpterygii <actionpterygii@gmail.com>
2019-03-15 00:32:50 +0900

    構造を大切にしようと思った

--- boyAndGirl.py
+++ boyAndGirl.py
@@ -2,7 +2,7 @@ import urllib3
 from bs4 import BeautifulSoup
 import certifi
 
-wish = "https://www.weblio.jp/ontology/%E6%97%A5%E6%9C%AC%E3%81%AE%E8%8A%B8%E8%83%BD%E4%BA%BA_1"
+wish = "http://www.kanji-jiten.jp/namelist/index22.html"
 
 
 
@@ -12,6 +12,10 @@ def parse(url):
     ca_certs=certifi.where())
     r = http.request('GET', url)
     soup = BeautifulSoup(r.data, 'html.parser')
-    return soup.select_one("#mainL > div.mainBoxB > div > div.subCatWordsL > div:nth-child(1) > a").string
+    # return soup.select_one("#main > [class=\"namaeyomi\"]:nth-child(2)").string
+    return soup.select_one("#main > [class=\"namaeyomi\"]:last-of-type").string
+    # return soup.select_one("#main > div:nth-child(8)").string
 
 print (parse(wish))
+
+
